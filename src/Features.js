7ffdbfe28fe937aeea48ef7d7436b49ea5f8324d
Jess Doyle <jessica.e.doyle@gmail.com>
2019-08-02 11:32:37 -0400

    header and featuresbroken off into disparate components

---
+++ src/Features.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function Features(props) {
+
+    const features = Object.keys(props.features)
+    .map(key => {
+      const options = props.features[key].map((item, index) => {
+        const selectedClass = item.name === props.state.selected[key].name ? 'feature__selected' : '';
+        const featureClass = 'feature__option ' + selectedClass;
+        return <li key={index} className="feature__item">
+          <div className={featureClass}
+            
+            onClick={e => props.updateFeature(key, item)}>
+              { item.name }
+              ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+                .format(item.cost) })
+          </div>
+        </li>
+      });
+
+      return <div className="feature" key={key}>
+        <div className="feature__name">{key}</div>
+        <ul className="feature__list">
+          { options }
+        </ul>
+      </div>
+    });
+
+
+    return (
+        <section className="main__form">
+            <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
+            { features }
+        </section>
+
+    );
+}
+
+export default Features;
---
+++ src/Summary.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Summary (props) {
+
+
+    return (
+        <div></div>
+    );
+}
